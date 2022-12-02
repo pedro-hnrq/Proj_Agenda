4618b9f0ded9fb8f19351d7e3d2f1f652b9b7f37
Pedro Feitosa <74242717+pedro-hnrq@users.noreply.github.com>
2022-12-01 22:08:44 -0300

    Projeto Agenda
    Faltando só font-end e implementação do cadastro usuário.

---
+++ core/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from core.models import Evento
+
+
+class EventoAdmin(admin.ModelAdmin):
+    list_display = ('id','titulo', 'data_evento', 'data_criacao')
+    
+
+
+admin.site.register(Evento, EventoAdmin)
